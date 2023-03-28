d5c79ac0294f7e04975edc8e4eb0717391a8a279
deepak <jangadeepak9@gmail.com>
2023-03-28 14:07:08 +0530

    Revert "making some changes in the file"
    This reverts commit 77caabadd1f2007c7a716fb63edca18ef25b4cd4.

--- main.java
+++ main.java
@@ -3,6 +3,5 @@ public class maian{
 public static void main(String ...args){
 
 systen.out.println("maintaing the healthy relationship")
-system.out.println("making some changes in the file main.java")
 }
 }
