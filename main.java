9c7e7d01e527ac50d87420f70732d9438b4a0275
deepak-ysh <128459612+deepak-ysh@users.noreply.github.com>
2023-03-28 14:01:01 +0530

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,6 @@ public class maian{
 
 public static void main(String ...args){
 
-systen.out.println("good warm welcome to the people")
+systen.out.println("maintaing the healthy relationship")
 }
 }
